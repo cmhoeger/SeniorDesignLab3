7fd66ca796a6f8b273a37f00bbb37b2e62f8f50a
cmhoeger <cmhoeger@uiowa.edu>
2022-04-14 20:21:08 -0500

    Added lots of stuff

--- seniorDesignLab3.py
+++ seniorDesignLab3.py
@@ -1,18 +1,34 @@
 
 import flask
 
-# A simple Flask App which takes
-# a user's name as input and responds
-# with "Hello {name}!"
-
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['GET', 'POST'])
+@app.route('/homepage.html', methods=['GET', 'POST'])
 def index():
     message = ''
     if flask.request.method == 'POST':
         message = 'Hello ' + flask.request.form['name-input'] + '!'
     return flask.render_template('homePage.html', message=message)
+
+@app.route('/connor.html')
+def connor():
+    return flask.render_template('connor.html')
+
+@app.route('/jt.html')
+def jt():
+    return flask.render_template('jt.html')
+
+@app.route('/brianna.html')
+def brianna():
+    return flask.render_template('brianna.html')
+
+@app.route('/joey.html')
+def joey():
+    return flask.render_template('joey.html')
+
+@app.route('/team_intro.html')
+def team_intro():
+    return flask.render_template('team_intro.html')
 
 if __name__ == '__main__':
     app.run()
